b6d7ae369729c6efd1dfb3b037c5acdd7f149c86
CoderGirl <angi.adema@gmail.com>
2023-10-02 09:58:41 -0600

    added the UserCard.jsx along with component code and added code to App.jsx in order to make the project render

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,18 @@
-function App() {}
+import { UserCard } from "./UserCard";
+import "./user.css";
+import user from "./user.json";
+
+function App() {
+  return (
+    <div>
+      <UserCard
+        name={user.name}
+        age={user.age}
+        phoneNumber={user.phoneNumber}
+        address={user.address}
+      />
+    </div>
+  );
+}
 
 export default App;
