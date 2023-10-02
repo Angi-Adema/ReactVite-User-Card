46e39ff3d1e9375b9a772ecec064b11e039746ad
CoderGirl <angi.adema@gmail.com>
2023-10-01 22:12:56 -0600

    reset the code back to original project so that I can rebuild the project myself

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,3 @@
-import "./user.css";
-import user from "./user.json";
-import { UserCard } from "./UserCard";
-
-function App() {
-  return (
-    <div>
-      <UserCard
-        name={user.name}
-        phoneNumber={user.phoneNumber}
-        age={user.age}
-        address={user.address}
-      />
-    </div>
-  );
-}
+function App() {}
 
 export default App;
